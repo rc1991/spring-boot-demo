81764cbf0f214ca45c7832afdc7fc2661e804227
rc1991 <rickphchen@gmail.com>
2019-05-02 19:26:51 +0800

    add jsp page to spring boot

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,17 +1,16 @@
 package hello;
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/hello")
-    public String index(@RequestParam(value="name", defaultValue="visitor") String name) {
-        HelloBO bo = new HelloBO(name);
-        return bo.getContent();
+    @GetMapping("/hello")
+    public String index(Model model, @RequestParam(value="name", required=false, defaultValue="visitor") String name) {
+        model.addAttribute("name", name);
+        return "hello/hello";
     }
 }
---
+++ src/main/webapp/WEB-INF/hello/hello.jsp
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+
+<html lang="en">
+Hello ${name}! This is a static page.
+</html>
